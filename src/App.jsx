511a56c082dc959c1fd29eac0b34c033312360cc
sagar777-900er <sagar.bagchi@iitg.ac.in>
2026-01-10 21:52:18 +0530

    Day 1: basic UI for CF handle input

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>Codeforces Analytics Dashboard</h1>
+
+      <input
+        type="text"
+        placeholder="Enter Codeforces handle"
+      />
+
+      <button>Analyze</button>
+    </div>
+  );
+}
+
+export default App;
