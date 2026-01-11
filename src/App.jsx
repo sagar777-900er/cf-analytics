206a331de288b41cf70ad1878f62c0777171f7f2
sagar777-900er <sagar.bagchi@iitg.ac.in>
2026-01-11 16:33:53 +0530

    Day 2: fetch and display Codeforces user info

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,33 @@
+import { useState } from "react";
+
 function App() {
+  const [handle, setHandle] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState("");
+
+  const fetchUser = async () => {
+    setError("");
+    setUserData(null);
+
+    try {
+      const res = await fetch(
+        `https://codeforces.com/api/user.info?handles=${handle}`
+      );
+
+      const data = await res.json();   
+
+      
+      if (data.status === "FAILED") {
+        setError("Invalid Codeforces handle");
+        return;
+      }
+
+      setUserData(data.result[0]);
+    } catch (err) {
+      setError("Something went wrong");
+    }
+  };
+
   return (
     <div>
       <h1>Codeforces Analytics Dashboard</h1>
@@ -6,9 +35,24 @@ function App() {
       <input
         type="text"
         placeholder="Enter Codeforces handle"
+        value={handle}
+        onChange={(e) => setHandle(e.target.value)}
       />
 
-      <button>Analyze</button>
+      <button onClick={fetchUser}>Analyze</button>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+{userData && (
+  <div>
+    <p><b>Handle:</b> {userData.handle}</p>
+    <p><b>Rating:</b> {userData.rating ?? "Unrated"}</p>
+    <p><b>Max Rating:</b> {userData.maxRating ?? "Unrated"}</p>
+    <p><b>Rank:</b> {userData.rank ?? "Unranked"}</p>
+  </div>
+)}
+
+
     </div>
   );
 }
